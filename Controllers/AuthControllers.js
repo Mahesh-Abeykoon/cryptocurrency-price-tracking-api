a15f73674f9af32537f68a2ca3825baa950d3805
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2024-01-03 19:56:11 +0530

    Implement user login

--- Controllers/AuthControllers.js
+++ Controllers/AuthControllers.js
@@ -20,3 +20,29 @@ module.exports.Signup = async (req, res, next) => {
         console.error(error);
         res.status(500).json({ message: "Internal server error" });
   }};
+
+module.exports.Login = async (req, res, next) => {
+
+    try {
+      const { email, password } = req.body;
+      if (!email || !password) {
+        return res.json({ message: 'All fields are required' });
+      }
+      
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.json({ message: 'Incorrect email' });
+      }
+      const auth = await bcrypt.compare(password, user.password); 
+      if (!auth) {
+        return res.json({ message: 'Incorrect password' });
+      }
+  
+      res.status(201).json({ message: "User logged in successfully", success: true });
+      next();
+      
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  };
--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -1,8 +1,8 @@
-const { Signup } = require("../Controllers/AuthControllers");
+const { Signup, Login } = require("../Controllers/AuthControllers");
 
 const router = require("express").Router();
 
 router.post("/signup", Signup);
-
+router.post("/login", Login)
 
 module.exports = router;
