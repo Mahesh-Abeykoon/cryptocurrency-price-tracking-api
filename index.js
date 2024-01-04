80f1b2e1ccfddf570259d246f3db2e22fcba22e3
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2024-01-04 18:52:25 +0530

    Update index.js to connect controllers and routes

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+const http = require('http');
+const websocketMiddleware = require('./Middlewares/WebsocketMiddleware');
+const authRoute = require('./Routes/AuthRoute');
+const cryptoRoute = require('./Routes/CryptoRoute');
 
-const authRoute = require("./Routes/AuthRoute");
+require('dotenv').config();
 
-require("dotenv").config()
 const app = express();
-
+const server = http.createServer(app);
 const PORT = 5001;
-const {MONGO_URL} = process.env;
+const { MONGO_URL } = process.env;
 
 mongoose.connect(MONGO_URL)
-.then(() => console.log("MongoDB is Connected Successfully"))
-.catch((error) => console.error(error));
+  .then(() => console.log('MongoDB is Connected Successfully'))
+  .catch((error) => console.error(error));
 
-app.listen(PORT, () =>{
-    console.log(`Server is listening on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 
 app.use(express.json());
+app.use(websocketMiddleware(server)); 
 
-app.use("/", authRoute);
+app.use('/', authRoute);
+app.use('/', cryptoRoute);
+
+module.exports = server;
