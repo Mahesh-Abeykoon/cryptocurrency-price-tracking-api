ebd4ad3119e74188b6d52cceef592b5e0bb75293
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2024-01-06 19:45:17 +0530

    Add condition for checking  API data

--- backend/util/FetchPredefinedCryptoDetails.js
+++ backend/util/FetchPredefinedCryptoDetails.js
@@ -8,6 +8,15 @@ async function fetchPredefinedCryptoDetails() {
       `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD&apikey=${API_KEY}`
     );
 
+    // Log the entire response data
+    //console.log('Response Data:', response.data);
+
+    // Check if response.data.Data exists and is an array
+    if (!response.data.Data || !Array.isArray(response.data.Data)) {
+      throw new Error('Invalid or missing data in the API response');
+    }
+
+    // Assuming data is an array, proceed with mapping
     const cryptoData = response.data.Data.map(crypto => ({
       name: crypto.CoinInfo.Name,
       symbol: crypto.CoinInfo.Name,
