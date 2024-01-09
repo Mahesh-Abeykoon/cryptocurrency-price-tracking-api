727537861b806d4ac5c3cda060c3b8ae78c4b087
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2024-01-09 23:02:06 +0530

    Updates

--- frontend/src/components/Crypto/CryptoListDetail.jsx
+++ frontend/src/components/Crypto/CryptoListDetail.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import api from './api';
+import api, { fetchPredefinedData } from './api';
 import { useCookies } from 'react-cookie';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
-
+import '../styles/CryptoListDetail.scss';
+import '@fortawesome/fontawesome-free/css/all.css';
+import Header from "../Header";
 const CryptoListDetail = () => {
   const navigate = useNavigate();
   const [cryptos, setCryptos] = useState([]);
   const [username, setUsername] = useState('');
   const [cookies, removeCookie] = useCookies([]);
-  // const [predefinedCryptos, setPredefinedCryptos] = useState([]);
+  const [predefinedCryptos, setPredefinedCryptos] = useState([]);
   const { id } = useParams();  
 
   const [webSocketData, setWebSocketdata] = useState([])
@@ -40,8 +42,8 @@ const CryptoListDetail = () => {
         setCryptos(response.data);
 
         // // Fetch predefined data initially
-        // const realTimeData = await fetchPredefinedData();
-        // setPredefinedCryptos(realTimeData);
+        const realTimeData = await fetchPredefinedData();
+        setPredefinedCryptos(realTimeData);
             
         //
         const socket = new WebSocket('ws://localhost:5001');
@@ -92,54 +94,80 @@ const CryptoListDetail = () => {
 
   return (
     <div>
-      {/* <div>
+    {/*  <div> 
         <h2>Top Cryptocurrencies</h2>
         <ol>
           {predefinedCryptos.map((crypto) => (
             <li key={crypto.name}>
-              <span><strong>Name:</strong> {crypto.name}</span>
+              {/* <span><strong>Name:</strong> {crypto.name}</span>
               <span><strong> Symbol:</strong> {crypto.symbol}</span>
-              <span><strong> Price:</strong> {crypto.current_price.toFixed(2)}</span>
-            </li>
-          ))}
-        </ol>
-      </div> */}
-    <div>
+              <span><strong> Price:</strong> {crypto.current_price.toFixed(2)}</span> */}
+              {/* <img src={crypto.image}></img> */}
+              {/* </li> */}
+               {/* ))}  */}
+         {/* </ol>   */}
+            {/*  </div> */}
+          
+    <Header username={username} onLogout={Logout} />
+        {/* {predefinedCryptos.map((cry) => (
+          <img key={cry.name} src={cry.image} alt={cry.name} />
+        ))}
+      */}
+    <div className="realtime-cryptos">
+      
       <h1>Top Cryptocurrencies</h1>
-      <h1>Your Component</h1>
       {webSocketData.length > 0 ? (
         <div>
           {webSocketData.map((crypto) => (
-            <div key={crypto.symbol} style={{ border: '1px solid #ccc', padding: '10px', margin: '10px' }}>
-              <h3>{crypto.name}</h3>
-              <p>Symbol: {crypto.symbol}</p>
-              <p>Price: {crypto.price}</p>
-              <p>Market Cap: {crypto.marketCap}</p>
-              <p>Change (24h): {crypto.change24h}</p>
-              {/* Add more information as needed */}
-            </div>
+            <ul key={crypto.symbol}>
+              <div>
+              {predefinedCryptos.map((predifined) => (
+                  predifined.symbol === crypto.symbol && (
+                <img key={predifined.name} src={predifined.image} alt={predifined.name} height="100" />
+                  )))}
+                <h3 className='cypto-name'>{crypto.name}</h3>
+                <h3 className='cypto-symbol'>{crypto.symbol}</h3>
+                <h3 className='cypto-price'>{crypto.price.toFixed(5)}</h3>
+              </div>
+            </ul>
           ))}
-        </div>
+      </div>
       ) : (
         <p>Waiting for WebSocket data...</p>
       )}
     </div>
-      <div>
-        <h2>Cryptocurrencies</h2>
-        <h4>Welcome <span>{username}</span></h4>
-        <button onClick={Logout}>Logout</button>
-        <ol>
-          {cryptos.map((crypto) => (
-            <li key={crypto._id}>
-              <span><strong>Name:</strong>{crypto.name}</span>
-              <span><strong> Symbol:</strong>{crypto.symbol}</span>
-              <span><strong> Price:</strong>{crypto.price} </span>
-              <button onClick={() => navigate(`/update/${crypto._id}`)}> Update</button>
-              <button onClick={() => handleDelete(crypto._id)}> X</button>
-            </li>
-          ))}
-        </ol>
-        {!id && <button onClick={() => navigate('/create-crypto')}>Add Crypto</button>}
+
+      <div className="user-cryptos">
+        <h2>Add Your Favourites Cryptos Here!..</h2>
+
+        {!id && <button onClick={() => navigate('/create-crypto')}>Add +</button>}
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Symbol</th>
+              <th>Price</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cryptos.map((crypto) => (
+              <tr key={crypto._id}>
+                <td>{crypto.name}</td>
+                <td>{crypto.symbol}</td>
+                <td>{crypto.price}</td>
+                <td>
+                  <button onClick={() => navigate(`/update/${crypto._id}`)}>
+                    <i className="fas fa-edit"></i>
+                  </button>
+                  <button onClick={() => handleDelete(crypto._id)}>
+                    <i className="fas fa-trash-alt"></i>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
       </div>
   );
