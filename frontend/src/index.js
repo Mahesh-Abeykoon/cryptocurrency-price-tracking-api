5be26a085ff8eed6b40b60615c3055a9980ee8c9
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2024-01-09 22:58:27 +0530

    Define footer in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '../src/components/App';
 import {BrowserRouter} from "react-router-dom";
-
+import Footer from "./components/Footer"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Footer/>
     </BrowserRouter>
    
   </React.StrictMode>
